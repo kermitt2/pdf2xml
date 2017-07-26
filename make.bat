eb9c150b2b9c8db9cf26e80e937c3446d2f4ad12
Luca Foppiano <luca@foppiano.org>
2017-07-26 18:04:06 +0200

    Try to build this crap

--- make.bat
+++ make.bat
@@ -2,28 +2,29 @@
 set DIRSRC=.\src
 set DIREXE=.\exe
 
-set DIRXPDF= ..\pdf2xml\xpdf_3.04\xpdf
-set DIRGOO=..\pdf2xml\xpdf_3.04\goo
-set DIRFOFI=..\pdf2xml\xpdf_3.04\fofi
-set DIRLIBXML=..\pdf2xml\libxml2-2.6.19+.win32
-set DIRICONV=..\pdf2xml\iconv-1.9.1.win32
+set DIRXPDF_ROOT=..\xpdf-3.04
+set DIRXPDF= ..\xpdf-3.04\xpdf
+set DIRGOO=..\xpdf-3.04\goo
+set DIRFOFI=..\xpdf-3.04\fofi
+set DIRLIBXML=..\libxml2-2.7.6.win32
+set DIRICONV=..\libiconv-1.15
 set CC=c:\mingw\bin\gcc.exe
 set CFLAGS=-g -O2 -I %DIRSRC% -I %DIRFOFI% -I %DIRGOO%
 set CXX=c:\mingw\bin\g++.exe
 set CXXFLAGS=%CFLAGS%
 set LIBPROG=c:\mingw\bin\ar.exe
 
-copy ..\pdf2xml\xpdf_3.04\aconf-dj.h %DIRSRC%\aconf.h
+copy %DIRXPDF_ROOT%\aconf-dj.h %DIRSRC%\aconf.h
 
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\ConstantsUtils.cc
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\ConstantsXML.cc
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\Parameters.cc
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\AnnotsXrce.cc
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\PDFDocXrce.cc
-%CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\XmlOutputdev.cc
-del %DIRSRC%\libsrc.a
+%CXX% %CXXFLAGS% -I%DIRXPDF% -I.\image\png -I.\image\zlib -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\XmlOutputdev.cc
+
+REM del %DIRSRC%\libsrc.a
 %LIBPROG% -rc %DIRSRC%\libsrc.a *.o
 
-%CXX% %CXXFLAGS% -o %DIREXE%\pdftoxml.exe %DIRSRC%\pdftoxml.cc  -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include %DIRLIBXML%\bin\libxml2.dll %DIRSRC%\libsrc.a %DIRXPDF%\libxpdf.a %DIRFOFI%\libfofi.a %DIRGOO%\libGoo.a
-
+%CXX% %CXXFLAGS% -o %DIREXE%\pdftoxml.exe %DIRSRC%\pdftoxml.cc -I.\image\png -I.\image\zlib\ -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include %DIRLIBXML%\bin\libxml2.dll %DIRSRC%\libsrc.a %DIRXPDF%\libxpdf.a %DIRFOFI%\libfofi.a %DIRGOO%\libGoo.a .\image\png\libpng.a
 
