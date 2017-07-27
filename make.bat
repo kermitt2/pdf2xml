3a493adc2b21b00e8a40ed6281b093a500bbcb7f
Luca Foppiano <luca@foppiano.org>
2017-07-27 10:16:13 +0200

    updating make.bat

--- make.bat
+++ make.bat
@@ -2,26 +2,29 @@
 set DIRSRC=.\src
 set DIREXE=.\exe
 
-set DIRXPDF_ROOT=..\xpdf-3.04
-set DIRXPDF= ..\xpdf-3.04\xpdf
-set DIRGOO=..\xpdf-3.04\goo
-set DIRFOFI=..\xpdf-3.04\fofi
+set XPDF=..\xpdf-3.04
 set DIRLIBXML=..\libxml2-2.7.6.win32
 set DIRICONV=..\libiconv-1.15
+set DIRPNG=.\image\png
+set DIRZLIB=.\image\zlib
+
+set DIRXPDF=%XPDF%\xpdf
+set DIRGOO=%XPDF%\goo
+set DIRFOFI=.%XPDF%\fofi
 set CC=c:\mingw\bin\gcc.exe
 set CFLAGS=-g -O2 -I %DIRSRC% -I %DIRFOFI% -I %DIRGOO%
 set CXX=c:\mingw\bin\g++.exe
 set CXXFLAGS=%CFLAGS%
 set LIBPROG=c:\mingw\bin\ar.exe
 
-copy %DIRXPDF_ROOT%\aconf-dj.h %DIRSRC%\aconf.h
+copy %XPDF%\aconf-dj.h %DIRSRC%\aconf.h
 
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\ConstantsUtils.cc
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\ConstantsXML.cc
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\Parameters.cc
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\AnnotsXrce.cc
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\PDFDocXrce.cc
-%CXX% %CXXFLAGS% -I%DIRXPDF% -I.\image\png -I.\image\zlib -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\XmlOutputdev.cc
+%CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRPNG%  -I%DIRZLIB% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\XmlOutputdev.cc
 
 REM del %DIRSRC%\libsrc.a
 %LIBPROG% -rc %DIRSRC%\libsrc.a *.o
