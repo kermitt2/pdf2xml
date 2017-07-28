b55008029df40b7adabb3f81336b30b943aad5ef
Luca Foppiano <luca@foppiano.org>
2017-07-28 17:24:19 +0200

    adding latest modification to the windows build

--- make.bat
+++ make.bat
@@ -14,10 +14,8 @@ set DIRFOFI=%XPDF%\fofi
 set CC=c:\mingw\bin\gcc.exe
 set CFLAGS=-g -O2 -I %DIRSRC% -I %DIRFOFI% -I %DIRGOO%
 set CXX=c:\mingw\bin\g++.exe
-set CXXFLAGS=%CFLAGS%
+set CXXFLAGS=%CFLAGS% -DHAVE_CONFIG_H
 set LIBPROG=c:\mingw\bin\ar.exe
-
-copy %XPDF%\aconf-dj.h %DIRSRC%\aconf.h
 
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\ConstantsUtils.cc
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\ConstantsXML.cc
@@ -26,8 +24,8 @@ copy %XPDF%\aconf-dj.h %DIRSRC%\aconf.h
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\PDFDocXrce.cc
 %CXX% %CXXFLAGS% -I%DIRXPDF% -I%DIRPNG%  -I%DIRZLIB% -I%DIRLIBXML%\include -I%DIRICONV%\include -c %DIRSRC%\XmlOutputdev.cc
 
-REM del %DIRSRC%\libsrc.a
+del %DIRSRC%\libsrc.a
 %LIBPROG% -rc %DIRSRC%\libsrc.a *.o
 
-%CXX% %CXXFLAGS% -o %DIREXE%\pdftoxml.exe %DIRSRC%\pdftoxml.cc -I.\image\png -I.\image\zlib\ -I%DIRXPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include %DIRLIBXML%\bin\libxml2.dll %DIRSRC%\libsrc.a %DIRXPDF%\libxpdf.a %DIRFOFI%\libfofi.a %DIRGOO%\libGoo.a .\image\png\libpng.a .\image\zlib\zlib.a
+%CXX% %CXXFLAGS% -o %DIREXE%\pdftoxml.exe %DIRSRC%\pdftoxml.cc -I%DIRPNG%  -I%DIRZLIB% -I%DIRXPDF% -I%XPDF% -I%DIRLIBXML%\include -I%DIRICONV%\include %DIRLIBXML%\bin\libxml2.dll %DIRSRC%\libsrc.a %DIRXPDF%\libxpdf.a %DIRFOFI%\libfofi.a %DIRGOO%\libGoo.a %DIRPNG%\libpng.a 
 
