5b1d33e2d90b073a6c32bab7de31d8da6e4d2b75
Luca Foppiano <luca@foppiano.org>
2017-08-01 10:58:59 +0200

    Final refinement of make.bat

--- make.bat
+++ make.bat
@@ -1,37 +1,33 @@
 
 set DIRSRC=.\src
 set DIREXE=.\exe
-
-set XPDF=..\xpdf-3.04
-REM set DIRLIBXML=..\libxml2-2.7.6.win32
-set DIRLIBXML=..\libxml2-2.7.8.win32
-set DIRICONV=..\libiconv-1.9.1
 set DIRPNG=.\image\png
 set DIRZLIB=.\image\zlib
 
+set DIRLIBXML=..\libxml2-2.7.8.win32
+set DIRICONV=..\libiconv-1.9.1
+
+set DIRXPDF_ROOT=..\xpdf-3.04
 set DIRXPDF=%XPDF%\xpdf
 set DIRGOO=%XPDF%\goo
 set DIRFOFI=%XPDF%\fofi
+
 set CC=vc.exe
 set CFLAGS=/O2 /I%DIRSRC% /I%DIRFOFI% /I%DIRGOO% /DHAVE_CONFIG_H
-
 set CXX=cl.exe
-set CXXFLAGS=%CFLAGS% /DHAVE_CONFIG_H
-set LIBPROG=lib
+set CXXFLAGS=%CFLAGS% 
+set LIBPROG=lib.exe
 
 %CXX% %CXXFLAGS% /I%DIRXPDF% /I%DIRLIBXML%\include /I%DIRICONV%\include -c %DIRSRC%\ConstantsUtils.cc
 %CXX% %CXXFLAGS% /I%DIRXPDF% /I%DIRLIBXML%\include /I%DIRICONV%\include -c %DIRSRC%\ConstantsXML.cc
 %CXX% %CXXFLAGS% /I%DIRXPDF% /I%DIRLIBXML%\include /I%DIRICONV%\include -c %DIRSRC%\Parameters.cc
-%CXX% %CXXFLAGS% /I%XPDF% /I%DIRXPDF% /I%DIRLIBXML%\include /I%DIRICONV%\include -c %DIRSRC%\AnnotsXrce.cc
-%CXX% %CXXFLAGS% /I%XPDF% /I%DIRXPDF% /I%DIRLIBXML%\include /I%DIRICONV%\include -c %DIRSRC%\PDFDocXrce.cc
-%CXX% %CXXFLAGS% /EHsc /I%XPDF% /I%DIRXPDF% /I%XPDF% /I%DIRPNG%  /I%DIRZLIB% /I%DIRLIBXML%\include /I%DIRICONV%\include -c %DIRSRC%\XmlOutputdev.cc
+%CXX% %CXXFLAGS% /I%DIRXPDF_ROOT% /I%DIRXPDF% /I%DIRLIBXML%\include /I%DIRICONV%\include -c %DIRSRC%\AnnotsXrce.cc
+%CXX% %CXXFLAGS% /I%DIRXPDF_ROOT% /I%DIRXPDF% /I%DIRLIBXML%\include /I%DIRICONV%\include -c %DIRSRC%\PDFDocXrce.cc
+%CXX% %CXXFLAGS% /EHsc /I%DIRXPDF_ROOT% /I%DIRXPDF% /I%DIRPNG%  /I%DIRZLIB% /I%DIRLIBXML%\include /I%DIRICONV%\include -c %DIRSRC%\XmlOutputdev.cc
 
 del %DIRSRC%\libsrc.lib
 %LIBPROG% /OUT:%DIRSRC%\libsrc.lib *.obj
 
 del *.obj
 
-REM %CXX% %CXXFLAGS% -o %DIREXE%\pdftoxml.exe %DIRSRC%\pdftoxml.cc /EHsc /I..\dirent\include /I%DIRPNG%  /I%DIRZLIB% /I%DIRXPDF% /I%XPDF% /I%DIRLIBXML%\include /I%DIRICONV%\include %DIRLIBXML%\bin\libxml2.dll %DIRSRC%\libsrc.a %DIRXPDF%\libxpdf.a %DIRFOFI%\libfofi.a %DIRGOO%\libGoo.a %DIRPNG%\libpng.a 
-
-
-%CXX% %CXXFLAGS% -o %DIREXE%\pdftoxml.exe %DIRSRC%\pdftoxml.cc /EHsc /I..\dirent\include /I%DIRPNG%  /I%DIRZLIB% /I%DIRXPDF% /I%XPDF% /I%DIRLIBXML%\include /I%DIRICONV%\include %DIRLIBXML%\lib\libxml2.lib %DIRSRC%\libsrc.lib %DIRXPDF%\libxpdf.lib %DIRFOFI%\fofi.lib %DIRGOO%\Goo.lib %DIRPNG%\libpng.lib advapi32.lib
+%CXX% %CXXFLAGS% -o %DIREXE%\pdftoxml.exe %DIRSRC%\pdftoxml.cc /EHsc /I..\dirent\include /I%DIRPNG%  /I%DIRZLIB% /I%DIRXPDF% /I%DIRXPDF_ROOT% /I%DIRLIBXML%\include /I%DIRICONV%\include %DIRLIBXML%\lib\libxml2.lib %DIRSRC%\libsrc.lib %DIRXPDF%\libxpdf.lib %DIRFOFI%\fofi.lib %DIRGOO%\Goo.lib %DIRPNG%\libpng.lib advapi32.lib
